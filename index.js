4d525f542f479ec7f488825d0ff25d8d1027e30c
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-04-02 10:56:40 -0400

    complete project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    //console.log(drivers);
+   
+     const result = drivers.filter(drive => drive == name || drive == name.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(drivers, name){
+   return drivers.filter(drive => drive.substring(0,2).includes(name))
+
+}
+
+function matchName(drivers, name){
+    return drivers.filter(drive => drive['name'] == name)
+}
